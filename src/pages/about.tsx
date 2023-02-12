888aa9d3f5460628bea4da3268c92770beed70ef
shaqok <shaqok8@gmail.com>
2023-02-12 03:23:40 -0600

    Initial setup for About page

---
+++ src/pages/about.tsx
@@ -0,0 +1,66 @@
+import React, { FC } from 'react'
+import { graphql } from 'gatsby'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
+import { siteType } from 'types/siteData.type'
+import Template from 'components/Common/Template'
+
+type AboutPageProps = {
+  data: {
+    site: siteType
+    file: {
+      childImageSharp: {
+        gatsbyImageData: IGatsbyImageData
+      }
+      publicURL: string
+    }
+  }
+}
+
+const About: FC<AboutPageProps> = ({
+  data: {
+    site: {
+      siteMetadata: { title, description, siteUrl, menuLinks },
+    },
+    file: {
+      childImageSharp: { gatsbyImageData },
+      publicURL,
+    },
+  },
+}) => {
+  console.log('menuLinks in about: ', menuLinks)
+  return (
+    <Template
+      title={title}
+      description={description}
+      siteUrl={siteUrl}
+      image={publicURL}
+      menuLinks={menuLinks}
+    >
+      <div>About Page</div>
+    </Template>
+  )
+}
+
+export default About
+
+export const getAboutPage = graphql`
+  query getAboutPageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+        siteUrl
+        menuLinks {
+          name
+          link
+        }
+      }
+    }
+    file(name: { eq: "profile-image" }) {
+      childImageSharp {
+        gatsbyImageData(width: 120, height: 120)
+      }
+      publicURL
+    }
+  }
+`
